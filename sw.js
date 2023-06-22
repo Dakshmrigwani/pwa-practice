c3bf99f6e17cd47fe163a585eff56aebae7bc69e
Daksh mrigwani <86098238+Dakshmrigwani@users.noreply.github.com>
2023-06-22 10:28:00 +0000

    showing manifest file and can download

--- sw.js
+++ sw.js
@@ -4,11 +4,11 @@ const assets = ["/", "/index.html", "/app.css", "/app.js"];
 
 self.addEventListener("install", (evt) => {
   console.log("service worker has been installed", evt);
-  evt.waitUntil(
-    caches.open(staticCacheName).then((cache) => {
-      cache.addAll(assets); // it stores all the data we want to store for our app
-    })
-  );
+//   evt.waitUntil(
+//     caches.open(staticCacheName).then((cache) => {
+//       cache.addAll(assets); // it stores all the data we want to store for our app
+//     })
+//   );
 });
 
 self.addEventListener("activate", (evt) => {
@@ -17,10 +17,10 @@ self.addEventListener("activate", (evt) => {
 //respondwith do top pause the fetch event and respond with the custom event
 self.addEventListener("fetch", (evt) => {
   //console.log('fetch event', evt);
-  evt.respondwith(
-    caches.match(evt.request).then((cacheRes) => {
-      return cacheRes || fetch(evt.request);
+//   evt.respondwith(
+//     caches.match(evt.request).then((cacheRes) => {
+//       return cacheRes || fetch(evt.request);
       // it helps to compare the event like if the item is not there then take from the server
-    })
-  );
+//     })
+//   );
 });
