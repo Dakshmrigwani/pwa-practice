9a6e1fad217f22262480dcd70e8ca52f0b9ad381
Daksh mrigwani <86098238+Dakshmrigwani@users.noreply.github.com>
2023-06-22 08:25:14 +0000

    code has been setup

--- sw.js
+++ sw.js
@@ -1,3 +1,10 @@
 self.addEventListener('install' , evt => { 
-	console.log("service worker has been installed")
+	console.log("service worker has been installed" , evt)
+})
+
+self.addEventListener('activate' , evt => {
+    console.log("service worker has been activated" , evt)
+})
+self.addEventListener('fetch' , evt => {
+    console.log("service worker has been fetched" , evt)
 })
