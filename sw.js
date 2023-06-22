51d21e4636fd2c3585053e1fef7acbd66af0694d
Daksh mrigwani <86098238+Dakshmrigwani@users.noreply.github.com>
2023-06-22 09:46:02 +0000

    added install and activate event

--- sw.js
+++ sw.js
@@ -1,5 +1,18 @@
+const staticCacheName = "static-cache-v1"
+
+const assets = [
+    '/',
+    '/index.html',
+    '/app.css',
+    '/app.js',
+]
+
 self.addEventListener('install' , evt => { 
 	console.log("service worker has been installed" , evt)
+    evt.waitUntil(
+        caches.open(staticCacheName).then(cache => {
+        cache.addAll(assets) // it stores all the data we want to store for our app
+        }))
 })
 
 self.addEventListener('activate' , evt => {
