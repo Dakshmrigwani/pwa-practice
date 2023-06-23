a9537288f88b2a4e08114724c7f5bd650bb627a7
Daksh mrigwani <86098238+Dakshmrigwani@users.noreply.github.com>
2023-06-23 12:11:11 +0000

    just some remove ,

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const staticCacheName = "static-cache-v6.50";
-const dynamicCacheName = "site-dynamic-v1.98";
+const staticCacheName = "static-cache-v6.51";
+const dynamicCacheName = "site-dynamic-v1.99";
 // we are storing the some data we can save that we have saved in assets variable
 const assets = [
   "/index.html",
