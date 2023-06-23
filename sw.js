1d87ce6afe02f1dd6dae793a2a022bd40aa38e3c
Daksh mrigwani <86098238+Dakshmrigwani@users.noreply.github.com>
2023-06-23 07:16:45 +0000

    limit the size

--- sw.js
+++ sw.js
@@ -1,32 +1,82 @@
-const staticCacheName = "static-cache-v1";
-const assets = ["/index.html", "/app.css", "/app.js"];
+const staticCacheName = "static-cache-v6.10";
+const dynamicCacheName = "site-dynamic-v1";
+const assets = [
+  "/",
+  "/index.html",
+  "/app.css",
+  "/app.js",
+  "/fallback.html",
+  "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css",
+  "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo.svg",
+  "https://fonts.googleapis.com/css?family=Poppins",
+  "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js",
+];
+
+// cache size limit function
+const limitCacheSize = (name, size) => {
+  caches.open(name).then((cache) => {
+    cache.keys().then((keys) => {
+      if (keys.length > size) {
+        cache.delete(keys[0]).then(limitCacheSize(name, size));
+      }
+    });
+  });
+};
 
 self.addEventListener("install", (evt) => {
   console.log("Service worker has been installed", evt);
   evt.waitUntil(
     caches.open(staticCacheName).then((cache) => {
-      return cache.addAll(assets); // Added "return" statement
+      return cache.addAll(assets);
     })
   );
 });
 
+// activate event
 self.addEventListener("activate", (evt) => {
-  console.log("Service worker has been activated", evt);
+  //console.log('service worker has been activated');
+  evt.waitUntil(
+    caches.keys().then((keys) => {
+      //console.log(keys);
+      return Promise.all(
+        keys
+          .filter(
+            (key) =>
+              key !== staticCacheName &&
+              key !== dynamicCacheName /*do not remove this cache as well*/
+          )
+          .map((key) => caches.delete(key))
+      );
+    })
+  );
 });
+// we are doing theat if the key is not equal then to staticCachedName which we have
+// changed and if not equal then it should be deleted and show the latest one
 
 self.addEventListener("fetch", (evt) => {
-  console.log('Fetch event', evt);
-  evt.respondWith(
-    caches.open(staticCacheName).then((cache) => { // Removed "const" before cache declaration
-      return cache.match(evt.request).then((cachedResponse) => { // Added "return" statement
-        if (cachedResponse !== undefined) {
-          // Cache hit, let's send the cached resource.
-          return cachedResponse;
-        } else {
-          // Nothing in cache, let's go to the network.
-          return fetch(evt.request); // Fetch from the network
+  console.log("fetch event", evt);
+  evt.respondwith(
+    caches
+      .match(evt.request)
+      .then((cacheRes) => {
+        return (
+          cacheRes ||
+          fetch(evt.request).then((fetchRes) => {
+            return caches.open(dynamicCacheName).then((cache) => {
+              cache.put(evt.request.url, fetchRes.clone());
+              limitCacheSize(
+                dynamicCacheName,
+                5
+              ); /*we are limiting the cache size*/
+              return fetchRes;
+            });
+          })
+        );
+      })
+      .catch(() => {
+        if (evt.request.url.indexOf(".html") > -1) {
+          return caches.match("/fallback.html");
         }
-      });
-    })
+      })
   );
 });
