c8212d960508ca96837b021e4db7d3850dfaf666
Daksh mrigwani <86098238+Dakshmrigwani@users.noreply.github.com>
2023-06-26 12:26:53 +0000

    adjust the three button while small screen

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const staticCacheName = "static-cache-v6.81";
-const dynamicCacheName = "site-dynamic-v2.38";
+const staticCacheName = "static-cache-v6.85";
+const dynamicCacheName = "site-dynamic-v2.42";
 // we are storing the some data we can save that we have saved in assets variable
 const assets = [
   "/index.html",
